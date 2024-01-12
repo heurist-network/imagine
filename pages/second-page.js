d76331f183077c2fe41019ff391107d9ffb695a0
flashcode <laughingliangcl@gmail.com>
2024-01-12 14:05:49 +0800

    feat: update

--- pages/second-page.js
+++
@@ -1,8 +0,0 @@
-// to detect language and automatically redirect to the approprate/[locale] page
-// import { Redirect } from '@/lib/redirect'
-// export default Redirect
-
-// to keep this root page with the defaultLocale
-import SecondPage, { getStaticProps } from './[locale]/second-page'
-export default SecondPage
-export { getStaticProps }
