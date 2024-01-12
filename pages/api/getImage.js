6f6c4f94111ff9294c4e9bdf667d202d1c6b049a
flashcode <laughingliangcl@gmail.com>
2024-01-13 01:43:37 +0800

    feat: update image page

--- pages/api/getImage.js
+++ pages/api/getImage.js
@@ -1,3 +1,46 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+const { setGlobalDispatcher, ProxyAgent } = require("undici");
+
+const detectEnvAndSetUrl = () => {
+  const isDev = process.env.NODE_ENV === 'development'
+  if (!isDev) return
+  const dispatcher = new ProxyAgent({ uri: new URL('http://127.0.0.1:7890').toString() });
+  setGlobalDispatcher(dispatcher);
+}
+detectEnvAndSetUrl()
+
+export default async function handler(req, res) {
+  console.log('req: ', req.body);
+  console.log('typeof req: ', typeof JSON.parse(req.body));
+  const obj = JSON.parse(req.body)
+  const model_input = {
+    prompt: obj.prompt,
+    num_iterations: obj.num_iterations,
+  }
+  const postData = {
+    "job_id": "job1000",
+    "model_input": {
+      "SD": {
+        "prompt": model_input.prompt,
+        "neg_prompt": "anime, bad hands, low quality, logo, artist name, distorted limbo, incorrect hands, incorrect arms",
+        "num_iterations": model_input.num_iterations,
+        "width": 512,
+        "height": 512
+      }
+    },
+    "model_type": "SD",
+    "model_id": "BlazingDrive",
+    "deadline": 60,
+    "priority": 1
+  }
+  const data = await fetch('http://70.23.102.189:3030/submit_job', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(postData)
+  }).then((res) => {
+    const url = res.text()
+    return url
+  })
+  console.log('data---', data);
+  const dataUrl = data.replaceAll('"', '')
+  res.status(200).json({ data: dataUrl, status: 200, message: 'ok' })
 }
--- pages/models/[model].js
+++ pages/models/[model].js
@@ -4,31 +4,86 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Image from "next/image";
 import { Button } from 'antd';
 import TopNav from '@/components/TopNav'
-
+import { Input } from 'antd';
+import { Slider } from 'antd';
 import style from "./index.module.scss";
 
 function Model({ model }) {
   const [data, setData] = useState("data Model");
-
+  const [value, setValue] = useState("")
+  const [num_iterations, setNumInterations] = useState(30)
+  // const [value, setValue] = useState("")
+  const [url, setUrl] = useState("")
+  const getImage = async () => {
+    const { data } = await fetch('/api/getImage', { method: 'POST', body: JSON.stringify({ prompt: 'girl', num_iterations }) }).then(res => res.json());
+    console.log('--res data--', data);
+    setUrl(data)
+  }
   useEffect(() => {
     console.log('-- model --', model);
+    // getImage()
   }, []);
+  const valueChange = (e) => {
+    console.log('设置value', e.target.value);
+    setValue(e.target.value)
+  }
   return (
     <div className={style.root}>
       <TopNav />
       <div className={style.root}>
+        <div className="content">
+          <div className="model_title_row">
+            <span className="model_title">{model}</span>
+          </div>
+          <div className="model_creator">
+            Created by
+          </div>
 
-      </div>
-      <div className='image-box'>
+          {model &&
+            <div className='image-box'>
+              <Image
+                width={512}
+                height={500}
+                priority
+                // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
+                src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
+                alt="模型图片"
+              /></div>}
 
-        <Image
-          width={512}
-          height={500}
-          priority
-          // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
-          src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
-          alt="模型图片"
-        />
+          <div className="input-form">
+            <div className="input-item">
+              <h3>Prompt</h3>
+              <Input value={value} placeholder="Prompt" size='large' onChange={valueChange} />
+            </div>
+            <div className="input-item">
+              <h3>Sampling Steps</h3>
+              <Slider min={1} value={num_iterations} onChange={value => setNumInterations(value)} max={50} defaultValue={30} disabled={false} />
+            </div>
+            {/* <div className="input-item">
+              <h3>Guidance Scale</h3>
+              <Slider min={1} value={scaleNumber} max={50} defaultValue={30} disabled={false} />
+            </div> */}
+            <div className="input-item">
+              <Button onClick={getImage} size='large' type='primary'>Submit</Button>
+            </div>
+
+            {url &&
+              <div className="input-item">
+                <div className='image-box'>
+                  <Image
+                    width={512}
+                    height={500}
+                    priority
+                    // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
+                    src={url}
+                    alt="模型图片"
+                  />
+                </div>
+              </div>
+            }
+          </div>
+
+        </div>
       </div>
     </div>
   )
