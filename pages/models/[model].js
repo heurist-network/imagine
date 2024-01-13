0dc2f51e3271e28f935aad89f4cc37750fb06f77
flashcode <laughingliangcl@gmail.com>
2024-01-13 17:42:44 +0800

    feat: style title

--- pages/models/[model].js
+++ pages/models/[model].js
@@ -1,5 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
+import Head from 'next/head';
+
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Image from "next/image";
 import { Button, Modal } from 'antd';
@@ -76,35 +78,39 @@ function Model({ model }) {
     setIsModalOpen(false);
   };
   return (
-    <div className={style.root}>
-      <TopNav />
+    <>
+      <Head>
+        <title>Heurist FREE Image generate</title>
+      </Head>
       <div className={style.root}>
-        <div className="content">
-          <div className="model_title_row">
-            <span className="model_title">{model}</span>
-          </div>
-          <div className="model_creator">
-            Created by
-          </div>
-          <Modal title="Prompt" open={isModalOpen} okText="Use this prompt" okButtonProps={{ size: 'large' }} cancelButtonProps={{ size: 'large' }} onOk={handleOk} onCancel={handleCancel}>
-            <p>{JSON.stringify(showInfo)}</p>
-          </Modal>
-          {model &&
-            <div className="image-wrap">
-              <div className='image-box'>
-                <Image
-                  width={512}
-                  height={500}
-                  priority
-                  // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
-                  src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
-                  alt="模型图片"
-                />
-                <div className="image-notice" onClick={() => showModal(model)}>
-                  <Image src='/info.png' width={20} height={20} alt="model info" />
+        <TopNav />
+        <div className={style.root}>
+          <div className="content">
+            <div className="model_title_row">
+              <span className="model_title">{model}</span>
+            </div>
+            <div className="model_creator">
+              Created by
+            </div>
+            <Modal title="Prompt" open={isModalOpen} okText="Use this prompt" okButtonProps={{ size: 'large' }} cancelButtonProps={{ size: 'large' }} onOk={handleOk} onCancel={handleCancel}>
+              <p>{JSON.stringify(showInfo)}</p>
+            </Modal>
+            {model &&
+              <div className="image-wrap">
+                <div className='image-box'>
+                  <Image
+                    width={512}
+                    height={500}
+                    priority
+                    // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
+                    src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
+                    alt="模型图片"
+                  />
+                  <div className="image-notice" onClick={() => showModal(model)}>
+                    <Image src='/info.png' width={20} height={20} alt="model info" />
+                  </div>
                 </div>
-              </div>
-              {/* <div className='image-box'>
+                {/* <div className='image-box'>
                 <Image
                   width={512}
                   height={500}
@@ -117,51 +123,53 @@ function Model({ model }) {
                   <Image src='/info.png' width={20} height={20} alt="模型图片" />
                 </div>
               </div> */}
-            </div>
-          }
-
-          <div className="input-form">
-            <div className="input-item">
-              <h3>Prompt</h3>
-              <Input value={value} placeholder="Prompt" size='large' onChange={valueChange} />
-            </div>
-            <div className="input-item">
-              <h3>Sampling Steps</h3>
-              <Slider
-                min={1}
-                max={50}
-                defaultValue={30}
-                // tooltip={{
-                //   open: false,
-                // }}
-                value={num_iterations}
-                onChange={value => setNumInterations(value)}
-                disabled={false}
-              />
-            </div>
-
-            <div className="input-item">
-              <Button onClick={getImage} loading={loading} size='large' type='primary'>SUBMIT</Button>
-            </div>
-
-            {url &&
-              <div className="input-item">
-                <div className='image-box'>
-                  <Image
-                    width={512}
-                    height={500}
-                    priority
-                    src={url}
-                    alt="模型图片1"
-                  />
-                </div>
               </div>
             }
-          </div>
 
+            <div className="input-form">
+              <div className="input-item">
+                <h3>Prompt</h3>
+                <Input value={value} placeholder="Prompt" size='large' onChange={valueChange} />
+              </div>
+              <div className="input-item">
+                <h3>Sampling Steps</h3>
+                <Slider
+                  min={1}
+                  max={50}
+                  defaultValue={30}
+                  // tooltip={{
+                  //   open: false,
+                  // }}
+                  value={num_iterations}
+                  onChange={value => setNumInterations(value)}
+                  disabled={false}
+                />
+              </div>
+
+              <div className="input-item">
+                <Button onClick={getImage} loading={loading} size='large' type='primary'>SUBMIT</Button>
+              </div>
+
+              {url &&
+                <div className="input-item">
+                  <div className='image-box'>
+                    <Image
+                      width={512}
+                      height={500}
+                      priority
+                      src={url}
+                      alt="模型图片1"
+                    />
+                  </div>
+                </div>
+              }
+            </div>
+
+          </div>
         </div>
       </div>
-    </div>
+    </>
+
   )
 }
 export default Model
