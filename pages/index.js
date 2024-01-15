381157097d1f5659c0f6ec03d7195405538720d7
flashcode <laughingliangcl@gmail.com>
2024-01-16 00:29:12 +0800

    feat: update

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,24 @@ import ImageBox from '@/components/ImageBox/Index.jsx'
 import ImageDemo from '@/components/ImageBox/demo'
 import TopNav from '@/components/TopNav'
 import style from "./index.module.scss";
+import Link from 'next/link';
 
 const Homepage = ({ imageJson }) => {
 
-  console.log('imageJson : --', imageJson);
   return (
     <>
       <Head>
-        <title>Heurist FREE Image generate</title>
+        <title>Imagine | FREE Image generator by Heurist</title>
       </Head>
       <main className={style.root}>
         <TopNav />
         <div className='content'>
-          <div className="welcome_text">Welcome to <span>Heurist</span></div>
+          <div className="welcome_text">Welcome to <span>Imagine</span> powered by Heurist</div>
           <h1>
-            We host best Stable Diffusion models on <span style={{ color: '#C57CFF' }}>fast GPUs</span> and offer them at <span style={{ color: '#FF7CCB' }}>low price</span>.
+            We host best Stable Diffusion models on <br></br><span style={{ color: '#C57CFF' }}>a decentralized network of GPUs</span>.
           </h1>
           <p className='text'>
-            <span> $0.0015 </span>to generate a 512x512 image. <span> 99.9% </span>reliability in last 30 days.
+            <span> Call for model creators: </span> Want to get your models listed and earn HUE token rewards? Contact <Link href='mailto:team@heurist.xyz' target="_blank">team@heurist.xyz</Link>
           </p>
           <ImageBox imageJson={imageJson} />
         </div>
@@ -38,8 +38,6 @@ export default Homepage
 export async function getStaticProps() {
   const res = await fetch('https://raw.githubusercontent.com/heurist-network/heurist-models/main/models.json')
   const imageJson = await res.json()
-  // const imageJson = []
-  // console.log('repo: --', imageJson);
   return {
     props: {
       imageJson
--- pages/models/[model].js
+++ pages/models/[model].js
@@ -9,28 +9,36 @@ import TopNav from '@/components/TopNav'
 import { Input } from 'antd';
 import { Slider } from 'antd';
 import { useClipboard } from '@/lib/tool'
+import Confetti from "react-confetti";
+
 import style from "./index.module.scss";
 const modelObj = {}
 
-function Model({ model }) {
+function Model({ model, author }) {
   const [data, setData] = useState("data Model");
   const [prompt, setPrompt] = useState("")
   const [neg_prompt, setNegPrompt] = useState("")
   const [num_iterations, setNumInterations] = useState(30)
-  const [seed, setSeed] = useState(0)
+  const [guidance_scale, setGuidanceScale] = useState(10)
+  const [seed, setSeed] = useState(-1)
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [showSuprise, setShowSuprise] = useState(false);
   const [showInfo, setShowInfo] = useState({})
+  const [width, setWidth] = useState(752)
+  const [height, setHeight] = useState(752)
   console.log('modelObj', modelObj);
   const [url, setUrl] = useState("")
   const getImage = async () => {
     setLoading(true)
+    // showSuprise = false;
     const { data } = await fetch('/api/getImage', {
-      method: 'POST', body: JSON.stringify({ prompt: 'girl', num_iterations, neg_prompt })
+      method: 'POST', body: JSON.stringify({ prompt: 'girl', num_iterations, neg_prompt, width, height, model, seed, neg_prompt })
     }).then(res => res.json());
     console.log('--res data--', data);
     setLoading(false)
     setUrl(data)
+    setShowSuprise(true)
   }
 
   const valueChange = (e) => {
@@ -39,12 +47,14 @@ function Model({ model }) {
   }
   const getImageInfo = async () => {
     const res1 = await fetch(`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.json`).then(res => res.json());
-    // const res2 = await fetch(`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-2.json`)
-    // const res3 = await fetch(`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-3.json`)
+    const res2 = await fetch(`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-2.json`).then(res => res.json());
+    const res3 = await fetch(`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-3.json`).then(res => res.json());
     modelObj[model] = res1
-    console.log('执行设置');
-    // modelObj[`${model}-2`] = res2
-    // modelObj[`${model}-3`] = res3
+    console.log('执行设置', res1);
+    modelObj[`${model}-2`] = res2
+    modelObj[`${model}-3`] = res3
+    // setShowSuprise(true)
+
   }
   useEffect(() => {
     if (model) {
@@ -60,6 +70,16 @@ function Model({ model }) {
         console.log('info: ', model);
         console.log('info -- : ', modelObj);
         setShowInfo(modelObj[model])
+        break;
+      case `${model}-2`:
+        console.log('info: ', model);
+        console.log('info -- : ', modelObj);
+        setShowInfo(modelObj[`${model}-2`])
+        break;
+      case `${model}-3`:
+        console.log('info: ', model);
+        console.log('info -- : ', modelObj);
+        setShowInfo(modelObj[`${model}-3`])
         break;
 
       default:
@@ -87,13 +107,16 @@ function Model({ model }) {
       </Head>
       <div className={style.root}>
         <TopNav />
+        {showSuprise && <Confetti numberOfPieces={3000} tweenDuration={8000} recycle={false} />}
+
         <div className={style.root}>
           <div className="content">
             <div className="model_title_row">
               <span className="model_title">{model}</span>
             </div>
             <div className="model_creator">
-              Created by Heurist
+              {/* TODO: use "author" field from https://raw.githubusercontent.com/heurist-network/heurist-models/main/models.json */}
+              Created by <span>{author}</span>
             </div>
             <Modal title="Prompt" open={isModalOpen} okText="Use this prompt" okButtonProps={{ size: 'large' }} cancelButtonProps={{ size: 'large' }} onOk={handleOk} onCancel={handleCancel}>
               <p>{JSON.stringify(showInfo)}</p>
@@ -105,56 +128,100 @@ function Model({ model }) {
                     width={512}
                     height={500}
                     priority
-                    // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
                     src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
-                    alt="模型图片"
+                    alt="sample"
                   />
                   <div className="image-notice" onClick={() => showModal(model)}>
                     <Image src='/info.png' width={20} height={20} alt="model info" />
                   </div>
                 </div>
-                {/* <div className='image-box'>
-                <Image
-                  width={512}
-                  height={500}
-                  priority
-                  // src="https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/ArthemyComics.png"
-                  src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.png`}
-                  alt="模型图片"
-                />
-                <div className="image-notice" onClick={() => showModal(`${model}-2`)}>
-                  <Image src='/info.png' width={20} height={20} alt="模型图片" />
+                <div className='image-box'>
+                  <Image
+                    width={512}
+                    height={500}
+                    priority
+                    src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-2.png`}
+                    alt="sample"
+                  />
+                  <div className="image-notice" onClick={() => showModal(`${model}-2`)}>
+                    <Image src='/info.png' width={20} height={20} alt="sample" />
+                  </div>
                 </div>
-              </div> */}
+
+                <div className='image-box'>
+                  <Image
+                    width={512}
+                    height={500}
+                    priority
+                    src={`https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-3.png`}
+                    alt="sample"
+                  />
+                  <div className="image-notice" onClick={() => showModal(`${model}-2`)}>
+                    <Image src='/info.png' width={20} height={20} alt="sample" />
+                  </div>
+                </div>
               </div>
             }
 
             <div className="input-form">
               <div className="input-item">
                 <h3>Prompt</h3>
-                <Input value={prompt} placeholder="Prompt" size='large' onChange={(value) => setPrompt(value)} />
+                <Input value={prompt} placeholder="Prompt" size='large' onChange={(e) => setPrompt(e.target.value)} />
               </div>
               <div className="input-item">
                 <h3>Sampling Steps</h3>
                 <Slider
                   min={1}
                   max={50}
-                  defaultValue={30}
-                  // tooltip={{
-                  //   open: false,
-                  // }}
+                  defaultValue={25}
                   value={num_iterations}
                   onChange={value => setNumInterations(value)}
                   disabled={false}
                 />
               </div>
               <div className="input-item">
+                <h3>Guidance Scale</h3>
+                <Slider
+                  min={1}
+                  max={20}
+                  step={0.1}
+                  defaultValue={7}
+                  value={guidance_scale}
+                  onChange={value => setGuidanceScale(value)}
+                  disabled={false}
+                />
+              </div>
+              <div className="input-item">
+                <h3>Width</h3>
+                <Slider
+                  min={512}
+                  max={1024}
+                  defaultValue={512}
+                  value={width}
+                  onChange={value => setWidth(value)}
+                  disabled={false}
+                />
+              </div>
+              <div className="input-item">
+                <h3>Height</h3>
+                <Slider
+                  min={512}
+                  max={1024}
+                  defaultValue={512}
+                  value={height}
+                  onChange={value => setHeight(value)}
+                  disabled={false}
+                />
+              </div>
+
+              {/* TODO: set width and height with slider. Range: 512 ~ 1024*/}
+              <div className="input-item">
                 <h3>Seed</h3>
-                <Input value={seed} placeholder="Seed" size='large' onChange={(value) => setSeed(value)} />
+                <Input value={seed} placeholder="Seed" size='large' onChange={(e) => setSeed(e.target.value)} />
               </div>
               <div className="input-item">
                 <h3>Negative Prompt</h3>
-                <Input value={neg_prompt} placeholder="Negative Prompt" size='large' onChange={(value) => setNegPrompt(value)} />
+                <Input value={neg_prompt} placeholder="Negative Prompt" size='large' onChange={(e) => setNegPrompt(e.target.value)} />
               </div>
               <div className="input-item">
                 <Button onClick={getImage} loading={loading} size='large' type='primary'>SUBMIT</Button>
@@ -168,7 +235,7 @@ function Model({ model }) {
                       height={500}
                       priority
                       src={url}
-                      alt="模型图片1"
+                      alt="sample1"
                     />
                   </div>
                 </div>
@@ -185,23 +252,18 @@ function Model({ model }) {
 export default Model
 export function getStaticPaths() {
   return {
-    paths: [
-      {
-        params: {
-          model: 'next.js',
-        },
-      }, // See the "paths" section below
-    ],
+    paths: [],
     fallback: true, // false or "blocking"
   }
 }
 export function getStaticProps({ params }) {
   const { model } = params;
-
+  const modelAndAuthor = model.split('-')
   console.log('model--', model);
   return {
     props: {
-      model
+      model: modelAndAuthor[0],
+      author: modelAndAuthor[1]
     }
   }
 }
