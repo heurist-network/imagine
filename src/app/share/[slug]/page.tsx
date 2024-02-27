53c6fae7b0421da9fb1592483d9111182def15bd
Ethan <ethan@zolplay.com>
2024-02-27 22:05:16 +0800

    fix: replace redirect to link

--- src/app/share/[slug]/page.tsx
+++ src/app/share/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Share() {
-  redirect("/");
+  // redirect("/");
 
-  return <div className="container">Redirect...</div>;
+  return (
+    <div className="container">
+      <Link href="/">Redirect to Index</Link>
+    </div>
+  );
 }
