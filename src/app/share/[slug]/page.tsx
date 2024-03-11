992a7c8a378332bf560d108639493ac295dc5901
Ethan <ethan@zolplay.com>
2024-03-11 10:53:35 +0800

    feat: display image in share page

--- src/app/share/[slug]/page.tsx
+++ src/app/share/[slug]/page.tsx
@@ -1,13 +1,39 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 
-export default function Share() {
+export default function Share({ params }: { params: { slug: string } }) {
+  const { slug } = params
+
+  console.log(slug, 'slug')
+
+  const url = `https://d1dagtixswu0qn.cloudfront.net/${slug}.png`
+
   return (
     <div className="container pt-16 text-center">
-      <Link href="/">
-        <Button variant="secondary">Redirect to Index</Button>
-      </Link>
+      <div className="mb-2 flex justify-center">
+        <Image
+          className="rounded-md"
+          src={url}
+          alt="imagine"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="flex justify-center gap-2">
+        <Link href="/">
+          <Button variant="secondary" size="sm">
+            Redirect to Index
+          </Button>
+        </Link>
+        <Link href={url} download>
+          <Button size="sm" variant="outline">
+            Download
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
