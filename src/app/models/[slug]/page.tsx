662d84fc38971de2c9002b274e2da3a33b705a52
ethan <ethan@zolplay.com>
2024-04-01 00:22:22 +0800

    fix: adjust the caching mechanism for obtaining the model resource list

--- src/app/models/[slug]/page.tsx
+++ src/app/models/[slug]/page.tsx
@@ -17,12 +17,21 @@ export default async function Models({ params }: { params: { slug: string } }) {
   try {
     const model1 = await fetch(
       `https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}.json`,
+      {
+        next: { revalidate: 60 },
+      },
     ).then((res) => res.json())
     const model2 = await fetch(
       `https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-2.json`,
+      {
+        next: { revalidate: 60 },
+      },
     ).then((res) => res.json())
     const model3 = await fetch(
       `https://raw.githubusercontent.com/heurist-network/heurist-models/main/examples/${model}-3.json`,
+      {
+        next: { revalidate: 60 },
+      },
     ).then((res) => res.json())
 
     const models = [
