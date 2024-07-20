20f5232cfbc15b9d4a755b29255448d619bb0e1d
Ethan <ethan@zolplay.com>
2024-07-21 00:23:37 +0800

    fix: add Suspense to fix useSearchParams build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import Image from 'next/image'
 
 import { OpacityWrapper } from '@/components/motion/opacityWrapper'
@@ -8,7 +9,9 @@ export default function Home() {
   return (
     <main className="flex-1">
       <div className="container">
-        <Hero />
+        <Suspense>
+          <Hero />
+        </Suspense>
         <OpacityWrapper>
           <div className="my-6 flex items-center gap-2 md:my-10">
             <div className="h-[1px] flex-1 bg-[#ebeaeb]" />
