ed07da78148dfee24a854bdbcd00272758cdb298
heurist-network <cyberpenguin3@protonmail.com>
2024-08-27 16:37:54 -0700

    remove old campaign page

--- src/app/campaign/page.tsx
+++ src/app/campaign/page.tsx
@@ -1,35 +1,29 @@
-import { notFound } from 'next/navigation'
+import { FeatureModel } from '@/modules/campaign/feature-model'
+import { Leaderboard } from '@/modules/campaign/leaderboard'
+import { CampaignPreview } from '@/modules/campaign/preview'
+import { CampaignReward } from '@/modules/campaign/reward'
 
-import { FeatureModels } from '@/modules/mint/featureModels'
-import { Leaderboard } from '@/modules/mint/leaderboard'
-import { Rewards } from '@/modules/mint/rewards'
+export default async function NewCampaign() {
+  const res: any[] = await fetch(
+    'https://raw.githubusercontent.com/heurist-network/heurist-models/main/models.json',
+    {
+      next: { revalidate: 3600 },
+    },
+  ).then((res) => res.json())
 
-export default async function Mint() {
-  try {
-    const res: any[] = await fetch(
-      'https://raw.githubusercontent.com/heurist-network/heurist-models/main/models.json',
-      {
-        next: { revalidate: 3600 },
-      },
-    ).then((res) => res.json())
+  const lists = res.filter(
+    (item) =>
+      item.type === 'sd15' ||
+      item.type === 'sdxl10' ||
+      item.type.includes('composite'),
+  )
 
-    const lists = res.filter(
-      (item) =>
-        item.type === 'sd15' ||
-        item.type === 'sdxl10' ||
-        item.type.includes('composite'),
-    )
-
-    return (
-      <main className="flex flex-1 flex-col pb-20">
-        <Rewards />
-
-        <FeatureModels lists={lists} />
-
-        <Leaderboard />
-      </main>
-    )
-  } catch (error) {
-    return notFound()
-  }
+  return (
+    <main className="flex-1">
+      <CampaignPreview />
+      <CampaignReward />
+      <FeatureModel lists={lists} />
+      <Leaderboard />
+    </main>
+  )
 }
