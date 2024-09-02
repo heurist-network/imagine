ccc0f3f5bc095be70b7dd7cec974c0415495541e
Ethan <ethan@zolplay.com>
2024-09-02 14:55:52 +0800

    feat: add hero animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
-import { Suspense } from 'react'
-import Image from 'next/image'
-
-import { OpacityWrapper } from '@/components/motion/opacityWrapper'
 import { FeatureModels } from '@/modules/featureModels'
 import { Hero } from '@/modules/hero'
-import { NewHero } from '@/modules/hero/new-index'
-import { Models } from '@/modules/models'
 
 export default function Home() {
   return (
     <main className="flex-1">
-      <NewHero />
+      <Hero />
       <FeatureModels />
     </main>
   )
 }
-// export default function Home() {
-//   return (
-//     <main className="flex-1">
-//       <div className="container">
-//         <Suspense>
-//           <Hero />
-//         </Suspense>
-//         <OpacityWrapper>
-//           <div className="flex my-6 gap-2 items-center md:my-10">
-//             <div className="bg-[#ebeaeb] flex-1 h-[1px]" />
-//             <Image src="/logo.svg" alt="logo" width={24} height={24} />
-//             <div className="bg-[#ebeaeb] flex-1 h-[1px]" />
-//           </div>
-//         </OpacityWrapper>
-//         <Models />
-//       </div>
-//     </main>
-//   )
-// }
--- src/components/magicui/text-generate-effect.tsx
+++ src/components/magicui/text-generate-effect.tsx
@@ -10,21 +10,31 @@ export const TextGenerateEffect = ({
   words1ClassName,
   words2,
   words2ClassName,
+  words3,
+  words3ClassName,
+  words4,
+  words4ClassName,
   className,
   filter = true,
   duration = 0.5,
 }: {
-  words1: string
+  words1?: string
   words1ClassName?: string
-  words2: string
+  words2?: string
   words2ClassName?: string
+  words3?: string
+  words3ClassName?: string
+  words4?: string
+  words4ClassName?: string
   className?: string
   filter?: boolean
   duration?: number
 }) => {
   const [scope, animate] = useAnimate()
-  let words1Array = words1.split(' ')
-  let words2Array = words2.split(' ')
+  let words1Array = words1?.split(' ') || []
+  let words2Array = words2?.split(' ') || []
+  let words3Array = words3?.split(' ') || []
+  let words4Array = words4?.split(' ') || []
   useEffect(() => {
     animate(
       'span',
@@ -61,6 +71,32 @@ export const TextGenerateEffect = ({
               <motion.span
                 key={word + idx}
                 className={cn('opacity-0', words2ClassName)}
+                style={{ filter: filter ? 'blur(10px)' : 'none' }}
+              >
+                {word}{' '}
+              </motion.span>
+            )
+          })}
+        </div>
+        <div>
+          {words3Array.map((word, idx) => {
+            return (
+              <motion.span
+                key={word + idx}
+                className={cn('opacity-0', words3ClassName)}
+                style={{ filter: filter ? 'blur(10px)' : 'none' }}
+              >
+                {word}{' '}
+              </motion.span>
+            )
+          })}
+        </div>
+        <div>
+          {words4Array.map((word, idx) => {
+            return (
+              <motion.span
+                key={word + idx}
+                className={cn('opacity-0', words4ClassName)}
                 style={{ filter: filter ? 'blur(10px)' : 'none' }}
               >
                 {word}{' '}
